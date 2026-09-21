e003d2c7d83b0a91d7c0d0149b565e4c8d2f547d
Päscu Simon <paescu@whatwedo.ch>
2026-09-21 07:22:20 +0200

    feat: remove kubectl since it's currently not part of our stack

--- container/scripts/install-packages.sh
+++ container/scripts/install-packages.sh
@@ -63,13 +63,6 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends 1passw
 # npm global tools
 npm install -g prettier
 
-# Install latest kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
-install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-rm kubectl kubectl.sha256
-
 # Install latest stable Neovim
 NVIM_TAG=$(curl -fsSL "https://api.github.com/repos/neovim/neovim/releases/latest" | grep '"tag_name"' | head -1 | sed 's/.*"tag_name": "\(.*\)".*/\1/')
 curl -fsSL "https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-linux-x86_64.tar.gz" -o /tmp/nvim.tar.gz
